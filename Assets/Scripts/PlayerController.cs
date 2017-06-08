31181c339f82130fc6c0d32ef4223b8c08d4af0b
Diego Augusto <daugustomoura@gmail.com>
2017-06-07 22:58:31 -0300

    restringindo posição da nave

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,31 @@
 public class PlayerController : MonoBehaviour
 {
     public float velocity = 15f;
+    public float padding = 1f;
+    public float xmin;
+    public float xmax;
 
-    // Use this for initialization
     void Start()
     {
-
+        var distance = transform.position.z - Camera.main.transform.position.z;
+        var leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        var rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(Time.deltaTime * -velocity, 0f, 0f);
+            transform.position += Vector3.left * velocity * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(Time.deltaTime * velocity, 0f, 0f);
+            transform.position += Vector3.right * velocity * Time.deltaTime;
         }
 
+        var newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.x);
     }
 }
