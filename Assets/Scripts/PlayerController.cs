923972729f377ab74b501e9015d17447df290dcd
Diego Augusto <daugustomoura@gmail.com>
2017-06-07 22:31:43 -0300

    criando movimento da nave

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(Time.deltaTime * -velocity, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(Time.deltaTime * velocity, 0f, 0f);
+        }
+
+    }
+}
