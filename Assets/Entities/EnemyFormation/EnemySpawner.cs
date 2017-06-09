7c2d01787c1729adeaba1d0671d4209b1f221534
Diego Augusto <daugustomoura@gmail.com>
2017-06-08 23:29:07 -0300

    criando formações para os inimigos

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+
+    void Start()
+    {
+
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
