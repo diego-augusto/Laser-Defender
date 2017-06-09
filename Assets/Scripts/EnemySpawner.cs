472cc51d65e3535f410f3b6fca6d3ee29a78f943
Diego Augusto <daugustomoura@gmail.com>
2017-06-08 23:08:46 -0300

    adicionando inimigo

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+
+    void Start()
+    {
+        GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        enemy.transform.parent = transform;
+    }
+
+    void Update()
+    {
+
+    }
+}
